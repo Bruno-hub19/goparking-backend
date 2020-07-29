af12a6fff39de9105718a5d14b82d79090320b0c
Bruno <brunnofn19@gmail.com>
2020-07-29 10:32:58 -0300

    add vehicles repository as dependency provider into containers index.ts

--- src/shared/containers/index.ts
+++ src/shared/containers/index.ts
@@ -5,7 +5,15 @@ import '@shared/containers/providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IVehiclesRepository from '@modules/vehicles/repositories/IVehiclesRepository';
+import VehiclesRepository from '@modules/vehicles/infra/typeorm/repositories/VehiclesRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IVehiclesRepository>(
+  'VehiclesRepository',
+  VehiclesRepository,
 );
