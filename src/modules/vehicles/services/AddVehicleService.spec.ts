2c4bdd083ad75d8c9191745d239bc95e2251ba93
Bruno <brunnofn19@gmail.com>
2020-07-29 10:32:07 -0300

    add add vehicle service and tests for it

---
+++ src/modules/vehicles/services/AddVehicleService.spec.ts
@@ -0,0 +1,88 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepostory';
+import FakeVehiclesRepository from '@modules/vehicles/repositories/fakes/FakeVehiclesRepository';
+
+import AddVehicleService from './AddVehicleService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeVehiclesRepository: FakeVehiclesRepository;
+
+let addVehicleService: AddVehicleService;
+
+describe('AddVehicle', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeVehiclesRepository = new FakeVehiclesRepository();
+
+    addVehicleService = new AddVehicleService(
+      fakeVehiclesRepository,
+      fakeUsersRepository,
+    );
+  });
+
+  it('should be able to add a new vehicle', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Linus',
+      email: 'torvalds@mail.com',
+      phone: '41999990000',
+      password: 'torvalds123',
+    });
+
+    const vehicle = await addVehicleService.execute({
+      license_plate: 'aaaa9999',
+      owner_id: user.id,
+    });
+
+    expect(vehicle).toEqual(
+      expect.objectContaining({
+        owner_id: user.id,
+        license_plate: 'aaaa9999',
+      }),
+    );
+  });
+
+  it('should not be able to add a new vehicle by pass a non-existing owner_id', async () => {
+    await expect(
+      addVehicleService.execute({
+        license_plate: 'aaaa9999',
+        owner_id: 'any-owner-id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to add a vehicle that already exists', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'Linus',
+      email: 'torvalds@mail.com',
+      phone: '41999990000',
+      password: 'torvalds123',
+    });
+
+    await addVehicleService.execute({
+      license_plate: 'aaaa9999',
+      owner_id: user1.id,
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'Elon',
+      email: 'musk@mail.com',
+      phone: '41999991111',
+      password: 'musk123',
+    });
+
+    await expect(
+      addVehicleService.execute({
+        license_plate: 'aaaa9999',
+        owner_id: user1.id,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+
+    await expect(
+      addVehicleService.execute({
+        license_plate: 'aaaa9999',
+        owner_id: user2.id,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ src/modules/vehicles/services/AddVehicleService.ts
@@ -0,0 +1,56 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IVehiclesRepository from '@modules/vehicles/repositories/IVehiclesRepository';
+
+import Vehicle from '@modules/vehicles/infra/typeorm/entities/Vehicle';
+
+interface IRequest {
+  license_plate: string;
+  owner_id: string;
+}
+
+@injectable()
+class AddVehicleService {
+  constructor(
+    @inject('VehiclesRepository')
+    private vehiclesRepository: IVehiclesRepository,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) { } // eslint-disable-line
+
+  public async execute({
+    license_plate,
+    owner_id,
+  }: IRequest): Promise<Vehicle> {
+    const findOwner = await this.usersRepository.findById(owner_id);
+
+    if (!findOwner) {
+      throw new AppError('This user does not exists');
+    }
+
+    const findVehicleByOwner = await this.vehiclesRepository.findByOwner(
+      owner_id,
+    );
+
+    const findVehicleByLicensePlate = await this.vehiclesRepository.findByLicensePlate(
+      license_plate,
+    );
+
+    if (findVehicleByOwner || findVehicleByLicensePlate) {
+      throw new AppError('This vehicle is already registered');
+    }
+
+    const vehicle = await this.vehiclesRepository.add({
+      license_plate,
+      owner_id,
+    });
+
+    return vehicle;
+  }
+}
+
+export default AddVehicleService;
