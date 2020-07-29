a9136b89fd30d323ca1efaf74bece499a1963dec
Bruno <brunnofn19@gmail.com>
2020-07-29 14:52:59 -0300

    remove ensure authentication middleware import

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 
 import UsersController from '@modules/users/infra/http/controllers/UsersController';
-// import ensureAuthentication from '@modules/users/infra/http/middlewares/ensureAuthentication';
 
 const userRouter = Router();
 
