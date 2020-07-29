409faf566990f67f98ab902b90c2abf0c336222f
Bruno <brunnofn19@gmail.com>
2020-07-29 15:14:40 -0300

    add new test into remove vehicle service spec file

--- src/modules/vehicles/services/RemoveVehicleService.spec.ts
+++ src/modules/vehicles/services/RemoveVehicleService.spec.ts
@@ -63,6 +63,34 @@ describe('RemoveVehicle', () => {
     ).rejects.toBeInstanceOf(AppError);
   });
 
+  it('should not be able to remove a vehicle from another user', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'Linus',
+      email: 'torvalds@mail.com',
+      phone: '41999990000',
+      password: 'torvalds123',
+    });
+
+    await fakeVehiclesRepository.add({
+      license_plate: 'aaaa9999',
+      owner_id: user1.id,
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'Elon',
+      email: 'musk@mail.com',
+      phone: '41999991111',
+      password: 'musk123',
+    });
+
+    await expect(
+      removeVehicleService.execute({
+        license_plate: 'aaaa9999',
+        owner_id: user2.id,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should not be able to remove a vehicle from an user that does not exist', async () => {
     await expect(
       removeVehicleService.execute({
