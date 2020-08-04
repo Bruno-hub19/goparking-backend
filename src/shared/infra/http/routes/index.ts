cfa4156db82ff2ceada87dd5a1190230ba849046
Bruno <brunnofn19@gmail.com>
2020-08-04 15:16:19 -0300

    add parking routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import usersRoutes from '@modules/users/infra/http/routes/users.routes';
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
 import vehiclesRoutes from '@modules/vehicles/infra/http/routes/vehicles.routes';
+import { parkingRoutes } from '@modules/parking/infra/http/routes/parking.routes';
 
 const routes = Router();
 
 routes.use('/user', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
 routes.use('/vehicles', vehiclesRoutes);
+routes.use('/parking', parkingRoutes);
 
 export default routes;
