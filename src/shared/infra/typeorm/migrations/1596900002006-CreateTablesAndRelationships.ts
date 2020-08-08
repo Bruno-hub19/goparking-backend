b9f00280cdaa2bf245451e09219f08a608040e29
Bruno <brunnofn19@gmail.com>
2020-08-08 12:22:15 -0300

    add new migration

--- src/shared/infra/typeorm/migrations/1596900002006-CreateTablesAndRelationships.ts
+++ src/shared/infra/typeorm/migrations/1596900002006-CreateTablesAndRelationships.ts
@@ -1,12 +1,15 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export default class CreateTablesAndInsertRelationship1596547302745
+export default class CreateTablesAndRelationships1596900002006
   implements MigrationInterface {
-  name = 'CreateTablesAndInsertRelationship1596547302745';
+  name = 'CreateTablesAndRelationships1596900002006';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "users_vehicles" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "license_plate" character varying NOT NULL, "owner_id" uuid NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_dc88e4ca12a5edd665e112bdd01" UNIQUE ("license_plate"), CONSTRAINT "PK_05e30f903c0920796ca4c4e5521" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "parking_lot" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "phone" character varying NOT NULL, "address" character varying NOT NULL, "five_minuts_price" numeric(10,2) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_fbd2ef5205aed868d6fbb0d8c80" UNIQUE ("name"), CONSTRAINT "UQ_fc792cc59ab75066a67aba6b437" UNIQUE ("email"), CONSTRAINT "UQ_07d3ea80d0e452d516f0b9a442b" UNIQUE ("phone"), CONSTRAINT "PK_b229703f7e14d1e22981fb35623" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "users_vehicles" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "license_plate" character varying NOT NULL, "owner_id" uuid NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_dc88e4ca12a5edd665e112bdd01" UNIQUE ("license_plate"), CONSTRAINT "PK_05e30f903c0920796ca4c4e5521" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "email" character varying NOT NULL, "phone" character varying NOT NULL, "password" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_a000cca60bcf04454e727699490" UNIQUE ("phone"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
@@ -22,5 +25,6 @@ export default class CreateTablesAndInsertRelationship1596547302745
     );
     await queryRunner.query(`DROP TABLE "users"`);
     await queryRunner.query(`DROP TABLE "users_vehicles"`);
+    await queryRunner.query(`DROP TABLE "parking_lot"`);
   }
 }
